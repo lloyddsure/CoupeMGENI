d469910eed992189fa1e2963b341b646b33f60f1
lloyddsure <virgil@DESKTOP-C3B5FSJ.localdomain>
2018-12-21 19:20:48 -0500

    correction

--- coupe_mgeni/coupe_mgeni/urls.py
+++ coupe_mgeni/coupe_mgeni/urls.py
@@ -14,4 +14,4 @@ urlpatterns = [
     path('blog/', include('blog.urls')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
