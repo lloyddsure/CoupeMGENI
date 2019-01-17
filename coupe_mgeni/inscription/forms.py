16d43d6b6e17f21e26bc34a18978dfd2c91a61d7
lloyddsure <virgil@DESKTOP-C3B5FSJ.localdomain>
2019-01-16 20:19:32 -0500

    add validators on inscription's form

--- coupe_mgeni/inscription/forms.py
+++ coupe_mgeni/inscription/forms.py
@@ -1,8 +1,10 @@
+import re
 from django import forms
 from .models import Equipe
 
 
 class InscriptionForm(forms.ModelForm):
+
     class Meta:
         model = Equipe
         fields = '__all__'
@@ -14,6 +16,41 @@ class InscriptionForm(forms.ModelForm):
             'emailContact': forms.EmailInput(attrs={'class': 'mdl-textfield__input'}),
             'telephoneContact': forms.TextInput(attrs={'class': 'mdl-textfield__input'}),
         }
+
+    def clean_nomEquipe(self):
+        nomEquipe = self.cleaned_data['nomContact']
+        if not re.match("^[0-9A-z\s\-\.]*$", nomEquipe):
+            raise forms.ValidationError("Pas de caractère spéciaux, svp !")
+
+        return nomContact  # Ne pas oublier de renvoyer le contenu du champ traité
+
+    def clean_nomContact(self):
+        nomContact = self.cleaned_data['nomContact']
+        if not re.match("^[A-z\s\-\.]*$", nomContact):
+            raise forms.ValidationError("Nom invalide !")
+
+        return nomContact  # Ne pas oublier de renvoyer le contenu du champ traité
+
+    def clean_prenomContact(self):
+        prenomContact = self.cleaned_data['prenomContact']
+        if not re.match("^[A-z\s\-\.]*$", prenomContact):
+            raise forms.ValidationError("Prénom invalide !")
+
+        return nomContact  # Ne pas oublier de renvoyer le contenu du champ traité
+
+    def clean_emailContact(self):
+        emailContact = self.cleaned_data['emailContact']
+        if not re.match(r"^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$", emailContact):
+            raise forms.ValidationError("Adresse couriel invalide !")
+
+        return emailContact  # Ne pas oublier de renvoyer le contenu du champ traité
+
+    def clean_telephoneContact(self):
+        telephoneContact = self.cleaned_data['telephoneContact']
+        if not re.match("[0-9]{10}",telephoneContact):
+            raise forms.ValidationError("Numéro de téléphone invalide !")
+
+        return telephoneContact  # Ne pas oublier de renvoyer le contenu du champ traité
 
 
 """
--- coupe_mgeni/inscription/views.py
+++ coupe_mgeni/inscription/views.py
@@ -9,19 +9,24 @@ def home(request):
 
 
 def  inscription(request):
-    # Construire le formulaire, soit avec les données postées,
-    # soit vide si l'utilisateur accède pour la première fois
-    # à la page.
-    print("Inscription")
 
-    form = InscriptionForm(request.POST or None)
-    # Nous vérifions que les données envoyées sont valides
-    # Cette méthode renvoie False s'il n'y a pas de données
-    # dans le formulaire ou qu'il contient des erreurs.
-    if form.is_valid():
-        # Ici nous pouvons traiter les données du formulaire
-        envoi = True
-        form.save()
+    if request.method == "POST":
+        # Construire le formulaire, soit avec les données postées,
+        # soit vide si l'utilisateur accède pour la première fois
+        # à la page.
+        print("Inscription")
 
-    # Quoiqu'il arrive, on affiche la page du formulaire.
-    return render(request, 'inscription.html', locals())
+        form = InscriptionForm(request.POST)
+        # Nous vérifions que les données envoyées sont valides
+        # Cette méthode renvoie False s'il n'y a pas de données
+        # dans le formulaire ou qu'il contient des erreurs.
+        if form.is_valid():
+            # Ici nous pouvons traiter les données du formulaire
+            envoi = True
+            form.save()
+
+        # Quoiqu'il arrive, on affiche la page du formulaire.
+        return render(request, 'inscription.html', locals())
+    else:
+        form = InscriptionForm(initial={})
+        return render(request, 'inscription.html', locals())
